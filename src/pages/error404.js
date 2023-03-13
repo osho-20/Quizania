9df79e409e0f1a42b40e403d5f478ded87581e24
YASH JAIN <yashjainiiita@gmail.com>
2023-03-14 00:42:44 +0530

    error404

---
+++ src/pages/error404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const error404 = () => {
+    return (
+        <div>
+            <h1>Oops!</h1>
+            <p>Page not found.</p>
+        </div>
+    )
+}
+
+export default error404
