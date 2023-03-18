0da6d690b0c4338770f466785aeb53521dba446a
YASH JAIN <yashjainiiita@gmail.com>
2023-03-18 21:21:51 +0530

    gallery finished

--- src/component/slider.js
+++ src/component/slider.js
@@ -18,7 +18,7 @@ export default function SimpleSlider(props) {
                 breakpoint: 1350,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 4,
+                    slidesToScroll: 3,
                 }
             },
             {
@@ -26,20 +26,22 @@ export default function SimpleSlider(props) {
                 settings: {
                     infinite: true,
                     slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToScroll: 2,
                 }
             },
             {
                 breakpoint: 600,
                 settings: {
+                    infinite: true,
                     slidesToShow: 2,
                     slidesToScroll: 2,
-                    initialSlide: 2
+                    initialSlide: 1
                 }
             },
             {
                 breakpoint: 400,
                 settings: {
+                    infinite: true,
                     slidesToShow: 1,
                     slidesToScroll: 1
                 }
